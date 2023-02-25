f7e3d8f603a52bfe408958991ea799a9a7d44dd6
wadiebouanfar <al415701@uji.es>
2023-02-25 22:55:16 +0100

    --> 4 tick test -->faltan 1 test.

--- src/main/java/table/RowWithLabel.java
+++ src/main/java/table/RowWithLabel.java
@@ -14,4 +14,5 @@ public class RowWithLabel extends Row {
         return numberClass;
     }
 
+
 }
--- src/test/java/csv/CSVTest.java
+++ src/test/java/csv/CSVTest.java
@@ -9,25 +9,38 @@ import table.TableWithLabels;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CSVTest {
-    private CSV csv;
+    private CSV csv ,csvW;
     private String nombreFichero;
 
     Table table;
     TableWithLabels tableWithLabels;
+
+    private List<Double> characteristics;
 
 
     //    Map<String, Integer> labelsToIndex = new HashMap<>();
     @BeforeEach
     void init() throws IOException {
         csv = new CSV();
+        csvW = new CSV();
 
+        nombreFichero ="/Users/wadiebouanfar/Documents/GitHub/practica1/csvFich/iris.csv";
+        tableWithLabels = csvW.readTableWithLabels(nombreFichero);
 
-        nombreFichero = "/Users/wadiebouanfar/Documents/GitHub/practica1/csvFich/irisSin.csv";
+        nombreFichero  = "/Users/wadiebouanfar/Documents/GitHub/practica1/csvFich/irisSin.csv";
         table = csv.readTable(nombreFichero);
+        characteristics  = new ArrayList<>();
+        characteristics.add(5.1);
+        characteristics.add(3.5);
+        characteristics.add(1.4);
+        characteristics.add(0.2);
+
 
 
     }
@@ -55,11 +68,18 @@ class CSVTest {
     @Test
     @DisplayName("Test de nombre de las etiquetas de las cabeceras")
     void testNameOfFlags() throws IOException {
-        nombreFichero = "/Users/wadiebouanfar/Documents/GitHub/practica1/csvFich/iris.csv";
-        tableWithLabels = csv.readTableWithLabels(nombreFichero);
+        System.out.println("Comprobando el metodo getRowAt().getNumberClass()");
+
         //Iris-setosa es 0 y ..1...2
 
-        assertEquals(0, csv.tableWithLabels.getRowAt(0).getNumberClass());
+        assertEquals(0, csvW.tableWithLabels.getRowAt(1).getNumberClass());
+
+    }
+    @Test
+    @DisplayName("Test de Datos ")
+    void testNumAsigned(){
+        System.out.println("Comprobando el metodo getRowAt().getData()");
+        assertEquals(characteristics,csvW.tableWithLabels.getRowAt(0).getData());
 
     }
 
